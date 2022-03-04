e8e6bed9b23e90bd3b5d2d24a6eacdb9fa93ec73
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-04 11:08:41 +0900

    showfile 修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,8 @@
 class Blog < ApplicationRecord
 
     with_options presence: true do
-        validates :title
-        validates :content
+            validates :title
+              validates :content
     end
     with_options length: { minimum: 1, maximum: 140} do
         validates :title
